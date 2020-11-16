3dd58d95f788980c0a3701c811e2c05df4f21c12
Milos Zaric <s80717@beuth-hochschule.de>
2020-11-16 17:59:02 +0100

    Update articles view
    + Loader
    + Removed thumbnail
    + Padding

--- src/templates/Articles.js
+++ src/templates/Articles.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import { Grid, Image, Card, Icon, Button } from 'semantic-ui-react';
+import { Grid, Image, Card, Icon, Label, Loader } from 'semantic-ui-react';
+
+import API from '../system/ApiConnector.js';
 
 class Article extends React.Component {
   render() {
     return (
-      <Card style={{ width: '100%' }} inverted="true">
-        <Image src='https://bilder.t-online.de/b/88/89/06/94/id_88890694/920/tid_da/donald-trump-will-den-wahrscheinlichen-wahlsieg-von-joe-biden-nicht-akzeptieren-der-us-praesident-spricht-von-wahlbetrug-.jpg' wrapped ui={false} />
-        <Card.Content>
-          <Card.Header>Donald Trump verliert US-Wahlen!</Card.Header>
-          <Card.Meta>11:31, 09.11.2020</Card.Meta>
+      <Card style={{ width: '100%' }}>
+        {/*<Image src='https://bilder.t-online.de/b/88/89/06/94/id_88890694/920/tid_da/donald-trump-will-den-wahrscheinlichen-wahlsieg-von-joe-biden-nicht-akzeptieren-der-us-praesident-spricht-von-wahlbetrug-.jpg' wrapped ui={false} />
+        */}<Card.Content>
+          <Card.Header as="a" href={"/article/" + this.props.article}>{this.props.title}</Card.Header>
+          <Card.Meta>{new Date(this.props.date).toLocaleDateString("de-DE")}</Card.Meta>
           <Card.Description>
-            Nun steht es endgültig fest [...]
+
+            {this.props.tags.map(function(tag){
+              return <Label key={tag}><Icon name='hashtag' />{tag}</Label>
+            })}
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
           <a style={{float: "left"}}>
             <Icon name='user' />
-            Max Mustermann
+            {this.props.author}
           </a>
+
+          <span style={{float: "right"}}>
+            <Icon name='comments' />
+            {this.props.comments}
+          </span>
 
           {/* <Button style={{float: "right"}}>Lesen</Button> */}
         </Card.Content>
@@ -27,31 +37,52 @@ class Article extends React.Component {
 }
 
 class Articles extends React.Component {
-  createTable() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: true,
+    };
+  }
 
+  componentDidMount() {
+    let comp = this;
+    let articles = [];
+
+    API.getBlog(function(blog) {
+        API.getArticles(blog.id, function(res) {
+          for (let i in res) {
+            console.log(res[i]);
+            articles.push(
+              <Grid.Column key={res[i].id} style={{marginBottom: "20px"}}>
+                <Article
+                  article={res[i].id}
+                  title={res[i].title}
+                  author={res[i].author.displayName}
+                  date={res[i].published}
+                  content={res[i].content}
+                  comments={res[i].replies.totalItems}
+                  tags={res[i].labels}
+                />
+              </Grid.Column>
+            );
+          }
+
+          comp.setState({
+            articles: articles,
+            loading: false,
+          });
+        });
+    });
   }
 
   render() {
     return (
       <Grid doubling columns={2} divided>
+        <Loader active={this.state.loading} />
+        
         <Grid.Row>
-          <Grid.Column>
-            <Article />
-          </Grid.Column>
-
-          <Grid.Column>
-            <Article />
-          </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row>
-          <Grid.Column>
-            <Article />
-          </Grid.Column>
-
-          <Grid.Column>
-            <Article />
-          </Grid.Column>
+          {this.state.articles}
         </Grid.Row>
       </Grid>
     );
