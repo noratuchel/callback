7f1eb715bc104bc025b671abcbae6d533e2119ee
Milos Zaric <s80717@beuth-hochschule.de>
2020-11-16 17:58:37 +0100

    Router update
    - 404 page
    - article page
    - site page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
 import Home from './pages/Home';
+import Page from './pages/Page';
+import Article from './pages/Article';
+
 import Navigation from './templates/Navigation';
 import Footer from './templates/Footer';
+// import API from './system/ApiConnector.js';
 
 function App() {
   return (
     <Router>
       <Navigation />
-      <Route exact path={["/", "/home"]} component={Home} />
-      <Route path="/clock" component={Clock} />
+      <Switch>
+        <Route exact path={["/", "/home"]} component={Home} />
+        <Route exact path='/page/:page' component={Page} />
+        <Route exact path='/article/:article' component={Article} />
+        <Route exact path='/404' />
+        <Redirect to="/404" />
+      </Switch>
       <Footer />
     </Router>
   );
